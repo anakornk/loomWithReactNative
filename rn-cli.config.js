1886423354e3bd5aeb9389acda42c7aafde7d03b
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-22 20:32:02 +0700

    added asyncstorage-down, vm-browserify, randombytes

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,6 +1,7 @@
 // rn-cli.config.js
 const nodeLibs = require('node-libs-react-native');
 nodeLibs.fs  = require.resolve('react-native-level-fs');
+nodeLibs.vm  = require.resolve('vm-browserify');
 
 module.exports = {
   resolver: {
