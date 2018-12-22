44a38385b21e91687a61530c4b7b7002f9068099
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-22 20:48:25 +0700

    successfully interact with plugin contract

--- App.js
+++ App.js
@@ -24,6 +24,17 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    (async function () {
+      const privateKey = CryptoUtils.generatePrivateKey()
+      const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey)
+      // console.log(privateKey);
+      const contract = await getContract(privateKey, publicKey)
+      await store(contract, '123', 'hello!')
+      const value = await load(contract, '123')
+      console.log('Value: ' + value)
+    })();
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -35,6 +46,59 @@ export default class App extends Component<Props> {
   }
 }
 
+// LOOM
+async function getContract(privateKey, publicKey) {
+  const client = new Client(
+    'default',
+    'ws://127.0.0.1:46657/websocket',
+    'ws://127.0.0.1:9999/queryws'
+  )
+
+  client.on(ClientEvent.Error, err => console.log(err || 'Unexpected Client Error'))
+
+  // required middleware
+  client.txMiddleware = [
+    new NonceTxMiddleware(publicKey, client),
+    new SignedTxMiddleware(privateKey)
+  ]
+  
+  const contractAddr = await client.getContractAddressAsync('BluePrint')
+    if (!contractAddr) {
+      throw new Error('Failed to resolve contract address')
+  }
+  const callerAddr = new Address(client.chainId, LocalAddress.fromPublicKey(publicKey))
+  return new Contract({
+    contractAddr,
+    callerAddr,
+    client
+  })
+}
+
+/**
+ * Stores an association between a key and a value in a smart contract.
+ * @param contract Contract instance returned from `getContract()`.
+ */
+async function store(contract, key, value) {
+  const params = new MapEntry()
+  params.setKey(key)
+  params.setValue(value)
+  await contract.callAsync('SetMsg', params)
+}
+
+/**
+ * Loads the value associated with a key in a smart contract.
+ * @param contract Contract instance returned from `getContract()`.
+ */
+async function load(contract, key) {
+  const params = new MapEntry()
+  // The smart contract will look up the value stored under this key.
+  params.setKey(key)
+  const result = await contract.staticCallAsync('GetMsg', params, new MapEntry())
+  return result.getValue()
+}
+
+
+// StyleSheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
